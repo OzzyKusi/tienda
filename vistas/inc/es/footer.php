5ba1e7becf11e9b5bec54690c5465753f3fe554a
OzzyKusi <83951848+OzzyKusi@users.noreply.github.com>
2021-05-10 05:16:04 -0500

    Add files via upload

---
+++ vistas/inc/es/footer.php
@@ -0,0 +1,54 @@
+<footer class="footer bg-white">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+                <p class="text-center lead" style="padding-bottom: 50px;">
+                    La tienda cuenta con una variedad exclusiva. Recibe primero las mejores ofertas. Ahorra más con los descuentos exclusivos ONLINE. Entérate de los nuevos productos destacados.
+                </p>
+            </div>
+
+            <div class="col-12 col-md-4 mb-4">
+                <ul class="list-unstyled">
+                    <li>
+                        <h5 class="font-weight-bold"><i class="far fa-copyright"></i> <?php echo COMPANY . " " . date("Y"); ?></h5>
+                    </li>
+                    <li> Todos los derechos reservados </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4 mb-4">
+                <ul class="list-unstyled">
+                    <li>
+                        <h5 class="font-weight-bold">CUSCO-PERU</h5>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4 mb-4">
+                <ul class="list-unstyled">
+                    <li>
+                        <h5 class="font-weight-bold">Sigueme en:</h5>
+                    </li>
+
+                    <li>
+                        Facebook
+                        </a>
+                    </li>
+
+                    <li>
+                        Instagram
+                        </a>
+                    </li>
+
+                    <li>
+                        Youtube
+                        </a>
+                    </li>
+                    <li>
+                        Twitter
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
